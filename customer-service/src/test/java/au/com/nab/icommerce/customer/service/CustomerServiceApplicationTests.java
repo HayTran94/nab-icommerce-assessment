129cd2ae82f882202662f97aa8ce72413b5f5ffe
TaiVT <taivt@vng.com.vn>
2021-04-16 21:06:26 +0700

    Add simple test for customer-service

--- customer-service/src/test/java/au/com/nab/icommerce/customer/service/CustomerServiceApplicationTests.java
+++ customer-service/src/test/java/au/com/nab/icommerce/customer/service/CustomerServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package au.com.nab.icommerce.customer.service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import au.com.nab.icommerce.customer.service.api.CustomerServiceGrpc;
+import au.com.nab.icommerce.customer.service.protobuf.PCustomer;
+import com.google.protobuf.Int32Value;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
-@SpringBootTest
+//@SpringBootTest
 class CustomerServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    //	@Test
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+                .usePlaintext()
+                .build();
+
+        CustomerServiceGrpc.CustomerServiceBlockingStub customerServiceBlockingStub = CustomerServiceGrpc.newBlockingStub(channel);
+
+        PCustomer pCustomer = customerServiceBlockingStub.getCustomerById(Int32Value.of(1));
+        System.out.println(pCustomer);
+
+//        PCustomer pCustomer = PCustomer.newBuilder()
+//                .build();
+//        Int32Value response = customerServiceBlockingStub.createCustomer(pCustomer);
+//        System.out.println(response);
+
+        channel.shutdown();
+    }
 
 }
