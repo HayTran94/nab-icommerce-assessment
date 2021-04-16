fd77d423a0a19e70623a2131a6496c8da02578bf
TaiVT <taivt@vng.com.vn>
2021-04-16 22:56:43 +0700

    Add write-product-service

---
+++ write-product-service/src/main/java/au/com/nab/icommerce/product/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package au.com.nab.icommerce.product.constant;
+
+public class AppConstant {
+    public static final int SUCCESS = 0;
+    public static final int FAILED = -1;
+
+}
---
+++ write-product-service/src/main/java/au/com/nab/icommerce/product/entity/Product.java
@@ -0,0 +1,37 @@
+package au.com.nab.icommerce.product.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "unit")
+    private String unit;
+
+}
