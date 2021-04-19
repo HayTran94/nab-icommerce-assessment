21770a5c3a8dc6f9f97d322ed277c41a1e62a950
TaiVT <taivt@vng.com.vn>
2021-04-19 13:41:13 +0700

    Update customer-auditing-service

---
+++ common-shared/src/main/java/au/com/nab/icommerce/common/constant/ErrorConstant.java
@@ -0,0 +1,7 @@
+package au.com.nab.icommerce.common.constant;
+
+public class ErrorConstant {
+    public static final int SUCCESS = 0;
+    public static final int FAILED = -1;
+
+}
---
+++ common-shared/src/main/java/au/com/nab/icommerce/common/mapper/AbstractProtobufMapper.java
@@ -0,0 +1,32 @@
+package au.com.nab.icommerce.common.mapper;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class AbstractProtobufMapper<E, P> {
+
+    public abstract E toDomain(P protobuf);
+
+    public List<E> toDomain(List<P> protobufs) {
+        if (protobufs == null) {
+            return Collections.emptyList();
+        }
+
+        return protobufs.stream()
+                .map(this::toDomain)
+                .collect(Collectors.toList());
+    }
+
+    public abstract P toProtobuf(E entity);
+
+    public List<P> toProtobuf(List<E> entities) {
+        if (entities == null) {
+            return Collections.emptyList();
+        }
+
+        return entities.stream()
+                .map(this::toProtobuf)
+                .collect(Collectors.toList());
+    }
+}
